9ae261ace2be6d693845b36c329c1b5e865271d4
Alice Bryer <alicebryer123@gmail.com>
2022-09-12 10:01:18 +0100

    added api routes

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
+"use strict";
+
 const express = require("express");
-const fs = require("fs");
-const noteData = require("./db/db.json");
 const app = express();
 const port = 3000;
-const path = require("path");
-const uniqid = require("uniqid");
-const router = require("express").Router;
-
-app.listen(process.env.PORT || port, () => {
-  console.log("app is listening at localhost");
-});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// post routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.post("/api/notes", (req, res) => {
-  const newNote = req.body;
-  newNote.id = uniqid();
-  noteData.push(newNote);
-  console.log(newNote);
-  fs.writeFileSync("db/db.json", JSON.stringify(noteData), (err) => {
-    if (err) throw err;
-    console.log("Saved!");
-  });
+app.listen(process.env.PORT || port, () => {
+  console.log("app is listening at localhost");
 });
