c6bfd181c9d6931dd1b430dd639637958394bc29
Alice Bryer <alicebryer123@gmail.com>
2022-09-12 08:34:33 +0100

    /notes & * routes added

--- server.js
+++ server.js
@@ -18,3 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
