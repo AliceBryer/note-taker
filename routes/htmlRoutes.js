b419148b3e08c7601943c3c8b5ad68ecc5a9b593
Alice Bryer <alicebryer123@gmail.com>
2022-09-12 10:24:17 +0100

    application working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,12 @@
 
 const path = require("path");
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
+module.exports = (app) => {
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-module.exports = htmlRoutes;
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
