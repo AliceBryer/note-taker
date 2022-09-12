7a05d99b5d400bcee7bdf044a33051dc1c99a7b2
Alice Bryer <alicebryer123@gmail.com>
2022-09-12 08:08:25 +0100

    set up & tested

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fs = require("fs");
+const noteData = require("./db/db.json");
+const app = express();
+const port = 3000;
+const path = require("path");
+
+app.listen(process.env.PORT || port, () => {
+  console.log("app is listening at localhost");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
